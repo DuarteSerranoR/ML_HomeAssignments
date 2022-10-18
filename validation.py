c99418252be80024fce59bd2b8e2998fe3f566cb
Duarte R <raposoduca@gmail.com>
2022-10-18 13:23:18 +0100

    added validation info

--- validation.py
+++ validation.py
@@ -1,4 +1,6 @@
 import numpy as np
+from sklearn import tree
+from itertools import product
 
 def simple_cross_validation(*, model, x, y, train_partition=(0, 0.8), metrics={"mae": mean_absolute_error}):
     n, m = x.shape
@@ -37,38 +39,57 @@ def kfold(*, model, x, y, k=5, metrics={"mae": mean_absolute_error}):
 def model_selection(
     model_type: str, # Classification or Regression
     x_train: np.ndarray, x_dev: np.ndarray, x_test: np.nd_array, 
-    y_train: np.ndarray, y_dev: np.ndarray, y_test: np.nd_array, 
+    y_train: np.ndarray, y_dev: np.ndarray, y_test: np.nd_array,
+    hyper_params_regression = {
+            "criterions": ["squared_error"],
+            "splitters": ["best"],
+            "max_depths": None,
+            "min_samples_splits": range(2,30,1),
+            "min_samples_leafs": range(1,30,1),
+            #"min_weight_fraction_leaf": [0.0],
+            "max_features": ["auto", "sqrt", "log2"]
+            #random_state: [int] [None] - none is default, it is int
+            #max_leaf_nodes: [int] [None] - none is default, it is int
+            #min_impurity_decrease: [0.0]
+            #ccp_alpha: [0.0]
+        }):
+    """
     hyper_params = {
             "criterions": ["squared_error", "friedman_mse", "absolute_error","poisson"],
             "splitters": ["best", "random"],
             "max_depths": range(10,100,10),
             "max_features": ["auto", "sqrt", "log2"],
-            "min_samples_leaf": range(1,30,1),
+            "min_samples_leafs": range(1,30,1),
             "min_samples_splits": range(2,30,1)
         }):
-    
-    # Split into train and test
-    n, m = x.shape
-    partition = int(0.8 * n)
-    x_train = x[:partition, :]
-    x_test = x[partition:, :]
-    y_train = y[:partition]
-    y_test = y[partition:]
+    """
     
     # Hyperparameters
-    criterions = ["squared_error"]
-    splitters = ["best"]
-    max_depths = [3, 4, 12, None]
-    hyper_parameters = product(criterions, splitters, max_depths)
-    
+    if model_type == "Regression":
+        criterion = hyper_params_regression["criterions"]
+        splitter = hyper_params_regression["splitters"]
+        max_depth = hyper_params_regression["max_depths"]
+        max_features = hyper_params_regression["max_features"]
+        min_samples_leaf = hyper_params_regression["min_samples_leaf"]
+        min_samples_splits = hyper_params_regression["min_samples_splits"]
+        hyper_parameters = product(criterion, splitter, max_depth, max_features, min_samples_leaf, min_samples_splits)
+    else:
+        raise NotImplementedError("Not Implemented")
+
+    # TODO - use enum for this, and do classification hyperparams
+
     results = {}
     
     for p in hyper_parameters:
-        criterion, splitter, max_depth = p
+        criterion, splitter, max_depth, max_features, min_samples_leaf, min_samples_splits = p
         
         model = tree.DecisionTreeRegressor(criterion=criterion,
                                            splitter=splitter,
-                                           max_depth=max_depth)
+                                           max_depth=max_depth,
+                                           max_features=max_features,
+                                           min_samples_leaf=min_samples_leaf,
+                                           min_samples_splits=min_samples_splits
+                                           )
         
         
         # Use either simple_cross_validation or kfold below
